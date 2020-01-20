c95673251c50fc62c47d3e20134deb19e03491aa
giussani.mirko <giussani.mirko@BIBLIO-PC06.OSDBSESTO.local>
2020-01-20 13:07:23 +0100

    add for circles

--- es 1/src/es/pkg1/Es1.java
+++ es 1/src/es/pkg1/Es1.java
@@ -9,15 +9,20 @@ package es.pkg1;
  *
  * @author giussani.mirko
  */
+import java.util.*;
 public class Es1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        do{
-            System.out.println("ciao prova");
-        }while(true);
+        Scanner input = new Scanner (System.in);
+        System.out.println("Inserisci n volte? ");
+        int volte = input.nextInt();
+        
+        for (int i = 0; i < volte; i++) {
+            System.out.println("ciao " + (i+1));
+        }
         
     }
     
